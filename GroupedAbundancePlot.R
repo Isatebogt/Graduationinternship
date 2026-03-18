cc1868441c212d41c8ce465791e4f0c42788a300
Isa te Bogt <113096082+Isatebogt@users.noreply.github.com>
2026-03-18 10:58:24 +0100

    Add files via upload
    made sure to add the right labels to the bars.

--- GroupedAbundancePlot.R
+++ GroupedAbundancePlot.R
@@ -1,50 +1,68 @@
 library(ggplot2)
 library(dplyr)
 library(ggiraph)
+library(ggh4x)
 
 make_species_plot <- function(metadata_df){
   
   metadata_df <- metadata_df %>%
     rename(any_of(c(Day = "Age", Genotype = "GenotypeIL22")))
   
-  
   metadata_df <- metadata_df %>%
     mutate(
       sample_label = paste(Day, sample, sep = "_"),
       bar_id = paste(sample_label, classid, sep = "_"),
-      x_label = paste(sample, GT, sep = "_"),  # keep for positioning,
     )
   
   metadata_df$Day <- factor(metadata_df$Day)
+  metadata_df$GT <- factor(metadata_df$GT)
   
-  p <- ggplot(metadata_df, aes(x = x_label, y = percentage)) + 
+  p <- ggplot(metadata_df, aes(x = sample, y = percentage)) + 
     geom_bar_interactive(
       aes(
         fill = classid,
         tooltip = paste0(
           "Sample: ", sample_label,
           "<br>Class: ", classid,
-          "<br>Percentage: ", round(percentage,2)
+          "<br>Percentage: ", round(percentage, 2)
         ),
         data_id = bar_id
       ),
       stat = "identity",
       hover_fill = "white"
     ) +
-    facet_wrap(~ Day, scales = "free_x", nrow = 1) +
+    facet_nested(                              
+      ~ Day + GT, 
+      scales = "free_x", 
+      space = "free_x",
+      strip = strip_nested(
+        background_x = list(
+          element_rect(fill = "lightgray", color = NA)
+        ),
+        text_x = list(
+          element_text(size = 10)                  
+        )
+      )
+    ) +
     scale_fill_viridis_d(option = "turbo") + 
     labs(
-      title = "composition per sample",
+      title = "Composition per sample",
       x = NULL,
       y = "Relative abundance (%)",
       fill = "Class ID"
     ) +
     theme(
+      panel.spacing = unit(0, "lines"),
+      strip.placement = "outside",
+      strip.background = element_blank(),
+      strip.clip = "off",                          # ← add this
+      axis.line = element_line(),
+      panel.grid.major.y = element_line(),
+      panel.background = element_rect(fill = "white", color = NA),
       axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
       axis.ticks.x = element_blank(),
-      strip.text = element_text(size = 12),
       legend.position = "right"
-    ) 
+    )
   
   girafe(
     ggobj = p,
@@ -54,5 +72,4 @@ make_species_plot <- function(metadata_df){
       opts_hover(css = "fill-opacity:0.6;stroke:grey;stroke-width:0.5px;cursor:pointer;")
     )
   )
-  
 }
